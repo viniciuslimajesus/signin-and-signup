aec556ab9503b73682fefa2c2d3f41c5cfa69581
viniciuslimajesus <vinicius.jesus.vl157@gmail.com>
2025-01-28 12:16:33 -0300

    Update

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+const btnSignin = document.querySelector('#signin')
+const btnSignup = document.querySelector('#signup')
+let body = document.querySelector('body')
+
+btnSignin.addEventListener('click', function () {
+    body.className = 'sign-in-js'
+})
+
+btnSignup.addEventListener('click', function () {
+    body.className = 'sign-up-js'
+})
